81565cbec4f7537c29b67bfe5b52c5c7ae21d3cb
ZbigniewKorycki <ZbigniewKorycki.info@gmail.com>
2023-08-09 11:20:43 +0200

    add function to create board

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-#  --- --- ---
-# | X | X | X |
-# | X | X | X |
-# | X | X | X |
-#  --- --- ---
-#
+# #  --- --- ---
+# # | X | X | X |
+#    --- --- ---
+# # | X | X | X |
+#    --- --- ---
+# # | X | X | X |
+# #  --- --- ---
+# #
 
-wall_up_down = [" --- --- --- "]
+wall_up_down = " --- --- --- "
+wall_back = "|"
+
+locations = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+
+def create_board():
+    print(wall_up_down)
+    for index, place in enumerate(locations, 1):
+        print(wall_back, end=" ")
+        print(place, end=" ")
+        if index % 3 == 0:
+            print(wall_back, end="\n")
+            print(wall_up_down)
+
+
+create_board()
+
+
+
+
+

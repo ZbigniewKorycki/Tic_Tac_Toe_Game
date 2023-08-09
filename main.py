8c59472706256d3ceb855fa3055703c6cd7fc3c7
ZbigniewKorycki <ZbigniewKorycki.info@gmail.com>
2023-08-09 13:40:00 +0200

    change winning locations to number, add players position list, add evaluation of winning

--- main.py
+++ main.py
@@ -6,14 +6,20 @@ locations = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 PLAYER_1_MARK = "*"
 PLAYER_2_MARK = "○"
 
-winning_locations = [[locations[0], locations[1], locations[2]],
-                    [locations[3], locations[4], locations[5]],
-                    [locations[6], locations[7], locations[8]],
-                    [locations[0], locations[3], locations[6]],
-                    [locations[1], locations[4], locations[7]],
-                    [locations[2], locations[5], locations[8]],
-                    [locations[0], locations[4], locations[8]],
-                    [locations[2], locations[4], locations[6]]]
+player_1_positions = []
+player_2_positions = []
+
+winning_locations = [
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"],
+    ["1", "4", "7"],
+    ["2", "5", "8"],
+    ["3", "6", "9"],
+    ["1", "5", "9"],
+    ["3", "5", "7"]
+]
+
 
 def create_board():
     print(wall_up_down)
@@ -25,30 +31,46 @@ def create_board():
             print(wall_up_down)
 
 
+def winning(player_positions):
+    for combination in winning_locations:
+        result_player = all(element in player_positions for element in combination)
+        if result_player:
+            return True
+    else:
+        return False
+
+
 game_on = True
 while game_on:
-    create_board()
     incorrect_move_player_1 = True
     while incorrect_move_player_1:
+        create_board()
         move_player_1 = input(f"Player 1: Under which position you want to put your mark ( {PLAYER_1_MARK} ): ")
         if move_player_1 in locations:
             position = locations.index(move_player_1)
+            player_1_positions.append(locations[position])
             locations[position] = PLAYER_1_MARK
             create_board()
             incorrect_move_player_1 = False
+
         else:
             print(f"This position is not available. Choose different one.")
-            create_board()
+    if winning(player_1_positions):
+        print("Player 1 win")
+        break
     incorrect_move_player_2 = True
     while incorrect_move_player_2:
+        create_board()
         move_player_2 = input(f"Player 2: Under which position you want to put your mark ( {PLAYER_2_MARK} ): ")
         if move_player_2 in locations:
             position = locations.index(move_player_2)
+            player_2_positions.append(locations[position])
             locations[position] = PLAYER_2_MARK
-            create_board()
             incorrect_move_player_2 = False
         else:
             print("This position is not available. Choose different one.")
-            create_board()
+    if winning(player_2_positions):
+        print("Player 2 win")
+        break
 
 
